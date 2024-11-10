927a6582279acefa1290f841e932ababc3df7f19
Ovesh Khan <164290281+Ovesh-khan@users.noreply.github.com>
2024-11-10 13:01:58 +0530

    Update server.js

--- cab-booking-backend/server.js
+++ cab-booking-backend/server.js
@@ -38,7 +38,7 @@ app.post("/inquiry", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT =process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
